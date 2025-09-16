4ee5971d9c70bbc46f1f77fa973c0d667cd13bed
JorgeMabuie <jorgemaabuie@gmail.com>
2025-09-16 23:29:57 +0200

    Update License.js

--- backend/models/License.js
+++ backend/models/License.js
@@ -3,9 +3,9 @@ const Schema = mongoose.Schema;
 
 const LicenseSchema = new Schema({
   key: { type: String, required: true, unique: true, index: true },
-  generatedForEmail: { type: String }, // optional to track who admin intended it for
+  generatedForEmail: { type: String }, // opcional, para rastrear para quem a chave foi gerada
   usedByUserId: { type: Schema.Types.ObjectId, ref: 'User' },
-  deviceId: { type: String }, // optional binding to a device
+  deviceId: { type: String }, // opcional, associar a um dispositivo específico
   activatedAt: Date,
   expiresAt: Date,
   active: { type: Boolean, default: true },
