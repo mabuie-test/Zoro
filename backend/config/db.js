a22faf4e6cb2f3f9ec5e988d6123d18989211ac4
JorgeMabuie <jorgemaabuie@gmail.com>
2025-09-18 22:45:38 +0200

    Delete backend directory

--- backend/config/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-async function connect(uri) {
-  await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-  console.log('MongoDB connected');
-}
-
-module.exports = { connect };
